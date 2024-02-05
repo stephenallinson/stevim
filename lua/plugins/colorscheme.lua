022e5f65ed50f5fee338dc6cb8fb996284ce63be
stephen.allinson <stephen.allinson@gmail.com>
2024-02-05 16:55:57 -0600

    testing some colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,54 @@
 return {
-    "navarasu/onedark.nvim",
-    lazy = false,
-    name = "onedark",
-    priority = 1000,
-    config = function()
-        require('onedark').setup {
-            style = "darker",
-            transparent = true,
-            term_colors = true,
-            ending_tildes = false,
-            code_style = {
-                comments = "none",
-            }
-        }
-        vim.cmd.colorscheme "onedark"
-        -- vim.cmd.highlight "Normal guibg=NONE"
-    end
+	{
+		dir = "/home/stephen/nvimproj/flex.nvim",
+		lazy = false,
+		name = "onedark",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({
+				style = "dark",
+				transparent = true,
+				term_colors = true,
+				ending_tildes = false,
+				code_style = {
+					comments = "none",
+				},
+			})
+			vim.cmd.colorscheme("onedark")
+		end,
+	},
+	-- {
+	-- 	"navarasu/onedark.nvim",
+	-- 	lazy = false,
+	-- 	name = "onedark",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("onedark").setup({
+	-- 			style = "darker",
+	-- 			transparent = true,
+	-- 			term_colors = true,
+	-- 			ending_tildes = false,
+	-- 			code_style = {
+	-- 				comments = "none",
+	-- 			},
+	-- 		})
+	-- 		vim.cmd.colorscheme("onedark")
+	-- 	end,
+	-- },
+	-- TODO: Keep up to date on this
+	-- {
+	-- 	"stephenallinson/flexoki-neovim",
+	-- 	name = "flexoki",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("flexoki").setup({
+	-- 			variant = "auto",
+	-- 			highlight_groups = {
+	-- 				base = {
+	--                        Comment = { italic = false },
+	-- 			},
+	-- 		})
+	-- 		vim.cmd.colorscheme("flexoki-dark")
+	-- 	end,
+	-- },
 }
--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,23 +1,55 @@
--- #TODO test
--- #todo test
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("todo-comments").setup({
+			merge_keywords = true,
 			keywords = {
+				FIX = {
+					icon = " ",
+					color = "error",
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+				},
 				TODO = { icon = " ", color = "info", alt = { "todo" } },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = "󱋆 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = "󰎚 ", color = "hint", alt = { "INFO" } },
+				TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+			gui_style = {
+				fg = "NONE",
+				bg = "BOLD",
 			},
 			highlight = {
 				comments_only = false,
 				multiline = false,
+				before = "fg",
+				after = "fg",
 				pattern = { [[#<(KEYWORDS)\s*]], [[.*<(KEYWORDS)\s*:]] },
 			},
 			colors = {
-				info = { "DiagnosticsInfo", "#CE5D97" },
+				-- FIX:: example
+				error = { "#AF3029" },
+
+				-- TODO: example
+				info = { "#205EA6" },
+
+				-- HACK: example
+				-- WARN: example
+				warning = { "#AD8301" },
+
+				-- NOTE: example
+				hint = { "#24837B" },
+
+				-- TEST: example
+				test = { "#A02F6F" },
+
+				-- PERF: example
+				default = { "#5E409D" },
 			},
 			search = {
-				pattern = [[\b(KEYWORDS)\b]],
+				pattern = [[\b(KEYWORDS):|#(KEYWORDS)\b]],
 			},
 		})
 		vim.keymap.set("n", "]t", function()
@@ -26,7 +58,6 @@ return {
 		vim.keymap.set("n", "[t", function()
 			require("todo-comments").jump_prev()
 		end, { desc = "Previous todo comment" })
-		-- TODO: test
 		vim.keymap.set("n", "<leader>tt", "<cmd>TodoTrouble<cr>", { desc = "Show in-code notes" })
 	end,
 }
