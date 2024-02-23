f57c6a7fad98be99ccc74e8ac171142d98af5e6f
stephen.allinson <stephen.allinson@gmail.com>
2024-02-23 16:06:31 -0600

    set global status for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,29 +1,30 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-        'linrongbin16/lsp-progress.nvim',
-    },
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = 'onedark',
-            component_separators = '|',
-            section_separators = '',
-        },
-        sections = {
-            lualine_c = {
-                {
-                    'filename',
-                    file_status = true,
-                    newfile_status = false,
-                    path = 4,
-                    symbols = {
-                        modified = '[+]',
-                        readonly = '[-]',
-                    }
-                }
-            },
-        },
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"linrongbin16/lsp-progress.nvim",
+	},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "onedark",
+			component_separators = "|",
+			section_separators = "",
+			globalstatus = true,
+		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					newfile_status = false,
+					path = 4,
+					symbols = {
+						modified = "[+]",
+						readonly = "[-]",
+					},
+				},
+			},
+		},
+	},
 }
