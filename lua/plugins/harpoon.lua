b655d268553e407c8fd1557e2e59ed53f5dc83af
stephen.allinson <stephen.allinson@gmail.com>
2024-02-23 15:52:05 -0600

    update harpoon based plugins

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 	config = function()
 		require("harpoon").setup({
 			menu = {
-				width = vim.api.nvim_win_get_width(0) - 4,
+				width = vim.api.nvim_win_get_width(0) - 40,
 			},
 		})
 		local mark = require("harpoon.mark")
