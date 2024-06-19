955df060c7ab8bae14df05c3dcab34a3f3cbb2da
stephen.allinson <stephen.allinson@gmail.com>
2024-06-19 14:09:47 -0500

    update autotag to new configuration

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,33 +1,35 @@
 return {
-    "windwp/nvim-ts-autotag",
-    config = function()
-        require("nvim-ts-autotag").setup({
-            enable = true,
-            enable_rename = true,
-            enable_close = false,
-            enable_close_on_slash = false,
+	"windwp/nvim-ts-autotag",
+	config = function()
+		require("nvim-ts-autotag").setup({
+			opts = {
+				enable = true,
+				enable_rename = true,
+				enable_close = false,
+				enable_close_on_slash = false,
 
-            filetypes = {
-                "html",
-                "javascript",
-                "typescript",
-                "javascriptreact",
-                "typescriptreact",
-                "svelte",
-                "vue",
-                "tsx",
-                "jsx",
-                "rescript",
-                "xml",
-                "php",
-                "markdown",
-                "astro",
-                "glimmer",
-                "handlebars",
-                "hbs",
-                "go",
-                "templ",
-            }
-        })
-    end
+				filetypes = {
+					"html",
+					"javascript",
+					"typescript",
+					"javascriptreact",
+					"typescriptreact",
+					"svelte",
+					"vue",
+					"tsx",
+					"jsx",
+					"rescript",
+					"xml",
+					"php",
+					"markdown",
+					"astro",
+					"glimmer",
+					"handlebars",
+					"hbs",
+					"go",
+					"templ",
+				},
+			},
+		})
+	end,
 }
