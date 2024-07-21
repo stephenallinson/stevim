2889d8e55938c5e4353eb2c18367086972875a55
stephenallinson <stephen.allinson@gmail.com>
2024-07-20 22:02:47 -0500

    set sane dap keymaps

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -118,6 +118,16 @@ return {
 			dap.listeners.before.event_exited["dapui_config"] = function()
 				require("dapui").close()
 			end
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F6>", dap.restart)
 		end,
 	},
 }
