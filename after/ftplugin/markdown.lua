5e9f4875f07564b0c01f5f35794bee961ae8465b
stephen.allinson <stephen.allinson@gmail.com>
2024-07-23 19:11:01 -0500

    move markdown folde

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,11 @@
+-- Markdown specific settings
+vim.opt.wrap = true -- Wrap text
+vim.opt.breakindent = true -- Match indent on line break
+vim.opt.linebreak = true -- Line break on whole words
+
+-- Spell check
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
+-- vim.keymap.set("i", "<Tab>", "<ESC>_>>A", { desc = "which_key_ignore", silent = true })
+-- vim.keymap.set("i", "<S-Tab>", "<ESC>_<<A", { desc = "which_key_ignore", silent = true })
