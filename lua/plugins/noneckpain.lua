985298c40e8e8d590f3c462f7e0610e9c0c4736c
stephen.allinson <stephen.allinson@gmail.com>
2024-12-23 13:51:06 -0600

    add noneckpain

---
+++ lua/plugins/noneckpain.lua
@@ -0,0 +1,10 @@
+return {
+	"shortcuts/no-neck-pain.nvim",
+	cmd = "NoNeckPain",
+	keys = {
+		{ "<leader>nn", "<CMD>NoNeckPain<CR>", desc = "[N]o [N]eckpain" },
+	},
+	opts = {
+		width = 150,
+	},
+}
