1e71fb1d59dfc14630ed4d5731078b2ea0aca1eb
Stephen Allinson <stephen.allinson@gmail.com>
2025-03-03 13:31:43 -0600

    remove local outline until ready for production

--- lua/plugins/local-outliner.lua
+++ lua/plugins/local-outliner.lua
@@ -1,8 +1,8 @@
 return {
-	"nvim-outliner",
-	dir = "~/luaproj/nvim-outliner/",
-	ft = "markdown",
-	config = function()
-		require("nvim-outliner").setup()
-	end,
+	-- "nvim-outliner",
+	-- dir = "~/luaproj/nvim-outliner/",
+	-- ft = "markdown",
+	-- config = function()
+	-- 	require("nvim-outliner").setup()
+	-- end,
 }
