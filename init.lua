acbbdfe6b06886888ee3571d47309845bce98f40
Stephen Allinson <stephen.allinson@gmail.com>
2025-05-10 17:55:54 -0500

    Add proper docker compose handling in init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Lazy Package Manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,13 +20,23 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- Let Neovim manage docker compose files
+vim.filetype.add({
+	filename = {
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		["compose.yml"] = "yaml.docker-compose",
+		["compose.yaml"] = "yaml.docker-compose",
+	},
+})
+
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 
 require("lazy").setup("plugins", {
-    change_detection = {
-        enabled = true,
-        notify = false,
-    },
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
 })
